d8c862768b920bc0b9d383e7a21e5df8dd30a2f0
suryadarmabelu-the <suryadarmabelu@gmail.com>
2026-07-27 01:49:43 +0800

    Delete Folder directory

--- Folder/supabaseClient.js
+++
@@ -1,9 +0,0 @@
-import { SUPABASE_URL, SUPABASE_ANON_KEY } from "./config.js";
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
-  auth: {
-    persistSession: true,
-    autoRefreshToken: true,
-  },
-});
